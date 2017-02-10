e08a30951d80ea39f385ef3e79ecf5dd16698ba6
Magdalena Banasiak <magdalena.banasiak@lakion.com>
2017-02-10 13:06:58 +0100

    Add getContainerLoader method to AppKernel

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -10,10 +10,21 @@
  */
 
 use Sylius\Bundle\CoreBundle\Application\Kernel;
+use Symfony\Component\Config\Loader\DelegatingLoader;
+use Symfony\Component\Config\Loader\LoaderResolver;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\DependencyInjection\Loader\ClosureLoader;
+use Symfony\Component\DependencyInjection\Loader\DirectoryLoader;
+use Symfony\Component\DependencyInjection\Loader\IniFileLoader;
+use Symfony\Component\DependencyInjection\Loader\PhpFileLoader;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\HttpKernel\Config\FileLocator;
 
 /**
  * @author Paweł Jędrzejewski <pawel@sylius.org>
  * @author Gonzalo Vilaseca <gvilaseca@reiss.co.uk>
+ * @author Magdalena Banasiak <magdalena.banasiak@lakion.com>
  */
 class AppKernel extends Kernel
 {
@@ -33,5 +44,23 @@ class AppKernel extends Kernel
         ];
 
         return array_merge(parent::registerBundles(), $bundles);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getContainerLoader(ContainerInterface $container)
+    {
+        $locator = new FileLocator($this, $this->getRootDir() . '/Resources');
+        $resolver = new LoaderResolver(array(
+            new XmlFileLoader($container, $locator),
+            new YamlFileLoader($container, $locator),
+            new IniFileLoader($container, $locator),
+            new PhpFileLoader($container, $locator),
+            new DirectoryLoader($container, $locator),
+            new ClosureLoader($container),
+        ));
+
+        return new DelegatingLoader($resolver);
     }
 }
