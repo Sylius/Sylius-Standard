c5ead1e396367bb8aa767dd4e6ee80740ed32552
Alexey <akorchaginn@gmail.com>
2017-06-12 18:42:25 +0300

    add admin news menu

---
+++ src/AppBundle/Menu/AdminMenuListener.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace AppBundle\Menu;
+
+use Sylius\Bundle\UiBundle\Menu\Event\MenuBuilderEvent;
+
+final class AdminMenuListener {
+    /**
+     * @param MenuBuilderEvent $event
+     */
+     public function addAdminMenuItems(MenuBuilderEvent $event)
+    {
+        $menu = $event->getMenu();
+
+        $newsMenu = $menu
+                ->addChild('new')
+                ->setLabel('app.ui.uncost')
+        ;
+
+        $newsMenu
+                ->addChild('new')
+                ->setLabel('app.ui.uncost')
+        ;
+    }
+}
