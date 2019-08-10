609a766b203bbd417c900ba855d0ccadb6881c4e
Paweł <pablos.lukowski@wp.pl>
2019-08-10 20:03:45 +0200

    clean mapping

--- src/Entity/Product/Product.php
+++ src/Entity/Product/Product.php
@@ -4,18 +4,13 @@ declare(strict_types=1);
 
 namespace App\Entity\Product;
 
-use Doctrine\ORM\Mapping as ORM;
+
 use Sylius\Component\Core\Model\Product as BaseProduct;
 use Sylius\Component\Product\Model\ProductTranslationInterface;
 
-/**
- * @ORM\Entity
- * @ORM\Table(name="sylius_product")
- */
+
 class Product extends BaseProduct
 {
-
-    /** @ORM\Column(type="string", nullable=true) */
     private $color;
 
     protected function createTranslation(): ProductTranslationInterface
@@ -23,7 +18,7 @@ class Product extends BaseProduct
         return new ProductTranslation();
     }
 
-    public function getColor()
+    public function getColor(): string
     {
         return $this->color;
     }
