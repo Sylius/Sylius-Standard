2a9868bf93754fc1c653305e3194d30e338a40a9
arti0090 <arti0090@gmail.com>
2020-12-16 11:37:25 +0100

    fix for new adjustment migration

--- src/Entity/Order/Adjustment.php
+++ src/Entity/Order/Adjustment.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace App\Entity\Order;
 
 use Doctrine\ORM\Mapping as ORM;
-use Sylius\Component\Order\Model\Adjustment as BaseAdjustment;
+use Sylius\Component\Core\Model\Adjustment as BaseAdjustment;
 
 /**
  * @ORM\Entity
