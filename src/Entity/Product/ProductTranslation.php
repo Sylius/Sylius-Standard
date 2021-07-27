07a40aac9b4dedf1c43ce59dc83cd7fd541d15d7
Wojciech Iracki <irackiw@gmail.com>
2021-07-27 15:32:29 +0200

    Refactor - add xml mapping

--- src/Entity/Product/ProductTranslation.php
+++ src/Entity/Product/ProductTranslation.php
@@ -4,13 +4,8 @@ declare(strict_types=1);
 
 namespace App\Entity\Product;
 
-use Doctrine\ORM\Mapping as ORM;
 use Sylius\Component\Core\Model\ProductTranslation as BaseProductTranslation;
 
-/**
- * @ORM\Entity
- * @ORM\Table(name="sylius_product_translation")
- */
 class ProductTranslation extends BaseProductTranslation
 {
 }
