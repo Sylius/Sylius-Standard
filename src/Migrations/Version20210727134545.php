0c370d2b9bf3da962325309fb2b65847c838665a
Wojciech Iracki <irackiw@gmail.com>
2021-07-27 15:48:44 +0200

    Add color property to ProductTranslation class + migration

---
+++ src/Migrations/Version20210727134545.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210727134545 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Add color property to sylius_product_translation';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE sylius_product_translation ADD color VARCHAR(50) DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE sylius_product_translation DROP color');
+    }
+}
