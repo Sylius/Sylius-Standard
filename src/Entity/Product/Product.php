1d1960c193a42beb5bc5d53f30fd48643e548823
thesylwio <sylwester.gorol1@gmail.com>
2021-11-18 22:56:22 +0100

    Map entity field into YAML

--- src/Entity/Product/Product.php
+++ src/Entity/Product/Product.php
@@ -4,18 +4,20 @@ declare(strict_types=1);
 
 namespace App\Entity\Product;
 
-use Doctrine\ORM\Mapping as ORM;
 use Sylius\Component\Core\Model\Product as BaseProduct;
-use Sylius\Component\Product\Model\ProductTranslationInterface;
 
-/**
- * @ORM\Entity
- * @ORM\Table(name="sylius_product")
- */
 class Product extends BaseProduct
 {
-    protected function createTranslation(): ProductTranslationInterface
+    /** @var string */
+    protected string $color;
+
+    public function getColor(): string
     {
-        return new ProductTranslation();
+        return $this->color;
+    }
+
+    public function setColor(string $color): void
+    {
+        $this->color = $color;
     }
 }
