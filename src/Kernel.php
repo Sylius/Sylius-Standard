2897d7348f4b36bc824e6648cca1c0f675b7cd94
Jakub Tobiasz <jakub.tobiasz@icloud.com>
2021-12-08 18:00:48 +0100

    Add missing polishsymfonycommunity/symfony-mocker-container dependency

--- src/Kernel.php
+++ src/Kernel.php
@@ -70,7 +70,7 @@ final class Kernel extends BaseKernel
 
     protected function getContainerBaseClass(): string
     {
-        if ($this->isTestEnvironment()) {
+        if ($this->isTestEnvironment() && class_exists(MockerContainer::class)) {
             return MockerContainer::class;
         }
 
