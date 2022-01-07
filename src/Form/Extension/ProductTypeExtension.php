cd95ce8349a0a7340de1c08d49d5d86edb1e2347
adamwjtk <adamwjtk@gmail.om>
2022-01-07 16:41:35 +0100

    changes

--- src/Form/Extension/ProductTypeExtension.php
+++ src/Form/Extension/ProductTypeExtension.php
@@ -5,22 +5,23 @@ declare(strict_types=1);
 namespace App\Form\Extension;
 
 use App\Form\Type\ProductColorType;
+use Sylius\Bundle\AdminApiBundle\Form\Type\ProductType;
 use Symfony\Component\Form\AbstractTypeExtension;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EnumType;
 use Symfony\Component\Form\FormBuilderInterface;
 
 final class ProductTypeExtension extends AbstractTypeExtension
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
-        $builder->add('color', TextType::class, [
-            'label' => 'app.form.extension.type.product.color',
+        $builder->add('color', EnumType::class, [
+            'label' => ProductColorType::class,
             'required' => true,
         ]);
     }
 
     public static function getExtendedTypes(): iterable
     {
-        return [ProductColorType::class];
+        return [ProductType::class];
     }
 }
--- src/Form/Type/ProductColorType.php
+++ src/Form/Type/ProductColorType.php
@@ -3,7 +3,7 @@
 namespace App\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EnumType;
 use Symfony\Component\Form\FormBuilderInterface;
 
 class ProductColorType extends AbstractType
@@ -11,6 +11,6 @@ class ProductColorType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('color', TextType::class);
+            ->add('color', EnumType::class);
     }
 }
