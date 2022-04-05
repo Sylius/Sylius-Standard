5f79f22c076ba0142bd75a0cbc4a9ca8a8945e20
Jakub Wegner <j.wegner@sunrisesystem.pl>
2022-04-05 16:27:49 +0200

    STYLE BitBag coding standard

--- src/Fixtures/ProductFixtures.php
+++ src/Fixtures/ProductFixtures.php
@@ -1,5 +1,12 @@
 <?php
 
+/*
+ * This file has been created by developers from BitBag.
+ * Feel free to contact us once you face any issues or want to start
+ * You can find more information about us on https://bitbag.io and write us
+ * an email on hello@bitbag.io.
+ */
+
 declare(strict_types=1);
 
 namespace App\Fixtures;
