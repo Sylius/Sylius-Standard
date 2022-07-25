2064d4714ffdbf779915cf5b5556ba10cf938535
Mateusz Zalewski <zaleslaw@gmail.com>
2022-07-25 10:37:04 +0200

    Add missing overridden Catalog Promotions-related models

---
+++ src/Entity/Promotion/CatalogPromotion.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Entity\Promotion;
+
+use Doctrine\ORM\Mapping as ORM;
+use Sylius\Component\Core\Model\CatalogPromotion as BaseCatalogPromotion;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="sylius_catalog_promotion")
+ */
+class CatalogPromotion extends BaseCatalogPromotion
+{
+}
---
+++ src/Entity/Promotion/CatalogPromotionScope.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Entity\Promotion;
+
+use Doctrine\ORM\Mapping as ORM;
+use Sylius\Component\Core\Model\CatalogPromotionScope as BaseCatalogPromotionScope;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="sylius_catalog_promotion_scope")
+ */
+class CatalogPromotionScope extends BaseCatalogPromotionScope
+{
+}
