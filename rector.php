253b4caf889a2603a0e16174d32b5cefd6a75abe
Jakub Tobiasz <jakub.tobiasz@icloud.com>
2022-07-29 13:38:10 +0200

    Configure recipes and Rector

---
+++ rector.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->import(__DIR__ . '/vendor/sylius/sylius-rector/config/config.php');
+    $rectorConfig->paths([
+        __DIR__ . '/src'
+    ]);
+};
