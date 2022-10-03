7af238c69ec66a8c42fb5ecc629c5f4d7a96fbf5
Ferror <zmalcherczyk@gmail.com>
2022-10-03 22:25:54 +0200

    [docker]Improve the error message by simply rendering json response

--- docker/test.sh
+++ docker/test.sh
@@ -14,6 +14,7 @@ do
     if [ "${i}" -gt "${timeout}" ]; then
 
         echo "Sylius Store was never created, aborting due to ${time}s timeout!"
+        curl -L http://localhost:80 -H Accept:application/json
         exit 1
     else
         echo "Sylius Store did not response"
