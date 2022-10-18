93d37e5967d4767959f9b23bbcaa5dc5ee43d3cf
Rafikooo <thecrazyrafik@gmail.com>
2022-10-18 23:57:41 +0200

    [Maintenance] rector.php scaffold introduced

---
+++ rector.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->importNames();
+    $rectorConfig->import(__DIR__ . '/vendor/sylius/sylius-rector/config/config.php');
+    $rectorConfig->paths([
+        __DIR__ . '/src'
+    ]);
+};
