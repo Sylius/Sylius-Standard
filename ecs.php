73fa9bae4393efe51c3fec0d398b37a62aa7f3bf
Rafikooo <thecrazyrafik@gmail.com>
2023-04-07 15:39:27 +0200

    [Maintenance] Add ECS path

--- ecs.php
+++ ecs.php
@@ -7,6 +7,7 @@ use Symplify\EasyCodingStandard\Config\ECSConfig;
 
 return static function (ECSConfig $config): void {
     $config->import('vendor/sylius-labs/coding-standard/ecs.php');
+    $config->paths(['src']);
 
     $config->skip([
         VisibilityRequiredFixer::class => ['*Spec.php'],
