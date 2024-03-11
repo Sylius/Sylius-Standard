ecbe113eaf03cbdf2dd1985ce5e9cc851389a338
Wojdylak <karolwojdyla37@gmail.com>
2024-03-11 15:49:25 +0100

    [Rector] Add remove unused imports

--- rector.php
+++ rector.php
@@ -6,6 +6,7 @@ use Rector\Config\RectorConfig;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->importNames();
+    $rectorConfig->removeUnusedImports();
     $rectorConfig->import(__DIR__ . '/vendor/sylius/sylius-rector/config/config.php');
     $rectorConfig->paths([
         __DIR__ . '/src'
