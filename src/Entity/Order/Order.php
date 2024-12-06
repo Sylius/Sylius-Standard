98261050599977e8d56286dfdc89c6e193183940
devdoodler <mszczep1@wit.edu.pl>
2024-12-06 01:29:58 +0100

    add to db and read admin note

--- src/Entity/Order/Order.php
+++ src/Entity/Order/Order.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Entity\Order;
 
+use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 use Sylius\Component\Core\Model\Order as BaseOrder;
 
@@ -11,4 +12,17 @@ use Sylius\Component\Core\Model\Order as BaseOrder;
 #[ORM\Table(name: 'sylius_order')]
 class Order extends BaseOrder
 {
+    #[ORM\Column(type: Types::TEXT, length: 500, nullable: true)]
+    private ?string $adminnotes;
+
+    public function getAdminnotes(): ?string
+    {
+        return $this->adminnotes;
+    }
+
+    public function setAdminnotes(string $adminnotes): void
+    {
+        $this->adminnotes = $adminnotes;
+    }
 }
+
