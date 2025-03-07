8802c8d99ab2f70157b0d78b2e90943118fb2558
Zupken <Zupken@spoko.pl>
2025-03-07 16:18:08 +0100

    Added field note to sylius_order

---
+++ migrations/Version20250307151648.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250307151648 extends AbstractMigration {
+    public function getDescription(): string {
+        return 'Added note field to sylius_order';
+    }
+
+    public function up(Schema $schema): void {
+        $this->addSql('ALTER TABLE sylius_order ADD note VARCHAR(500) DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void {
+        $this->addSql('ALTER TABLE sylius_order DROP note');
+    }
+}
--- src/Entity/Order/Order.php
+++ src/Entity/Order/Order.php
@@ -9,6 +9,15 @@ use Sylius\Component\Core\Model\Order as BaseOrder;
 
 #[ORM\Entity]
 #[ORM\Table(name: 'sylius_order')]
-class Order extends BaseOrder
-{
+class Order extends BaseOrder {
+    #[ORM\Column(type: "string", nullable: true, length: 500)]
+    private ?string $note = null;
+
+    public function getNote(): ?string {
+        return $this->note;
+    }
+
+    public function setNote(?string $note): void {
+        $this->note = $note;
+    }
 }
