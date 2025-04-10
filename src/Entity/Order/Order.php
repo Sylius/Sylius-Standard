1024c7d35d8fe83fa5f734d1d7fda99e8f2cd1ad
TheMilek <kam.grygierzec@gmail.com>
2025-04-10 14:50:01 +0200

    Update base entities

--- src/Entity/Order/Order.php
+++ src/Entity/Order/Order.php
@@ -6,6 +6,11 @@ namespace App\Entity\Order;
 
 use Doctrine\ORM\Mapping as ORM;
 use Sylius\Component\Core\Model\Order as BaseOrder;
+use Sylius\MolliePlugin\Entity\AbandonedEmailOrderTrait;
+use Sylius\MolliePlugin\Entity\MolliePaymentIdOrderTrait;
+use Sylius\MolliePlugin\Entity\OrderInterface;
+use Sylius\MolliePlugin\Entity\QRCodeOrderTrait;
+use Sylius\MolliePlugin\Entity\RecurringOrderTrait;
 
 /**
  * @ORM\Entity
@@ -13,6 +18,10 @@ use Sylius\Component\Core\Model\Order as BaseOrder;
  */
 #[ORM\Entity]
 #[ORM\Table(name: 'sylius_order')]
-class Order extends BaseOrder
+class Order extends BaseOrder implements OrderInterface
 {
+    use AbandonedEmailOrderTrait;
+    use RecurringOrderTrait;
+    use QRCodeOrderTrait;
+    use MolliePaymentIdOrderTrait;
 }
