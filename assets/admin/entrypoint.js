9b36b8d79826a48236ac45b8cb8d333023cc7eb7
Rafikooo <thecrazyrafik@gmail.com>
2025-05-08 19:27:44 +0200

    WIP

--- assets/admin/entrypoint.js
+++ assets/admin/entrypoint.js
@@ -1,2 +1,4 @@
 // In this file you can import assets like images or stylesheets
 console.log('Hello Webpack Encore! Edit me in assets/admin/entrypoint.js');
+
+import '../../vendor/sylius/multi-source-inventory-plugin/assets/admin/entrypoint';
--- config/bundles.php
+++ config/bundles.php
@@ -62,4 +62,6 @@ return [
     Symfony\UX\Icons\UXIconsBundle::class => ['all' => true],
     FOS\RestBundle\FOSRestBundle::class => ['all' => true],
     Sylius\PayPalPlugin\SyliusPayPalPlugin::class => ['all' => true],
+    Sylius\GmvBundle\SyliusGmvBundle::class => ['all' => true],
+    Sylius\MultiSourceInventoryPlugin\SyliusMultiSourceInventoryPlugin::class => ['all' => true],
 ];
