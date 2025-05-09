f9da088136f649910cd0044660c2fa0306365a13
Rafikooo <thecrazyrafik@gmail.com>
2025-05-09 11:45:07 +0200

    WIP

--- src/Command/ProjectWizardCommand.php
+++ src/Command/ProjectWizardCommand.php
@@ -131,7 +131,7 @@ PHP;
 
 
 
-        // Multi Source Inventory Plugin
+        // Loyalty Plugin
         if (isset($data['plugins']['sylius/loyalty-plugin'])) {
             $io->section('Recreating rector.php for Loyalty plugin');
             $rectorFile = getcwd() . '/rector.php';
@@ -175,6 +175,56 @@ PHP;
                 return Command::FAILURE;
             }
         }
+
+
+
+
+
+        // Return Plugin
+        if (isset($data['plugins']['sylius/return-plugin'])) {
+            $io->section('Recreating rector.php for Return plugin');
+            $rectorFile = getcwd() . '/rector.php';
+
+            // Remove old file
+            if (file_exists($rectorFile)) {
+                $filesystem->remove($rectorFile);
+                $io->text('Removed existing rector.php');
+            }
+
+            // Create new rector.php with required set
+            $newContent = <<<'PHP'
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Sylius\SyliusRector\Set\SyliusPlus;
+
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->importNames();
+    $rectorConfig->removeUnusedImports();
+    $rectorConfig->import(__DIR__ . '/vendor/sylius/sylius-rector/config/config.php');
+    $rectorConfig->paths([
+        __DIR__ . '/src'
+    ]);
+    $rectorConfig->sets([SyliusPlus::RETURN_PLUGIN]);
+};
+PHP;
+
+            $filesystem->dumpFile($rectorFile, $newContent);
+            $io->text('Created new rector.php with RETURN_PLUGIN set');
+
+            // Run Rector
+            $io->section('Running Rector for Return');
+            $rectorProc = new Process(['vendor/bin/rector']);
+            $rectorProc->setTty(Process::isTtySupported());
+            $rectorProc->run();
+            if (!$rectorProc->isSuccessful()) {
+                $io->error('Rector run failed: ' . $rectorProc->getErrorOutput());
+                return Command::FAILURE;
+            }
+        }
+
 
         // Final common steps
         // Remove existing cache directories to avoid stale container errors
