2b2a84ef8837dd707f038341ca3539260903d03b
Rafikooo <thecrazyrafik@gmail.com>
2025-05-13 16:14:35 +0200

    WIP

--- rector.php
+++ rector.php
@@ -6,10 +6,15 @@ use Rector\Config\RectorConfig;
 use Sylius\SyliusRector\Set\SyliusPlus;
 
 return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->import(__DIR__ . '/vendor/sylius/sylius-rector/config/config.php');
+    $rectorConfig->paths([__DIR__ . '/src']);
+
+    foreach (glob(__DIR__ . '/vendor/sylius/*/rector.php') as $pluginRectorConfig) {
+        /** @var callable $configurePlugin */
+        $configurePlugin = require $pluginRectorConfig;
+        $configurePlugin($rectorConfig);
+    }
+
     $rectorConfig->importNames();
     $rectorConfig->removeUnusedImports();
-    $rectorConfig->import(__DIR__ . '/vendor/sylius/sylius-rector/config/config.php');
-    $rectorConfig->paths([
-        __DIR__ . '/src'
-    ]);
 };
