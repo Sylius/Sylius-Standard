dff7a61897d01676519e482ff1f9d375152cd4a8
Rafikooo <thecrazyrafik@gmail.com>
2025-05-14 08:29:38 +0200

    Revert "WIP"
    This reverts commit d384c7a0f19386821128f0f4414e82ad06703440.

--- assets/admin/entrypoint.js
+++ assets/admin/entrypoint.js
@@ -1,4 +1,2 @@
 // In this file you can import assets like images or stylesheets
 console.log('Hello Webpack Encore! Edit me in assets/admin/entrypoint.js');
-
-import '@vendor/sylius/b2b-kit/assets/admin/entrypoint';
--- assets/shop/entrypoint.js
+++ assets/shop/entrypoint.js
@@ -1,4 +1,2 @@
 // In this file you can import assets like images or stylesheets
 console.log('Hello Webpack Encore! Edit me in assets/shop/entrypoint.js');
-
-import '@vendor/sylius/b2b-kit/assets/shop/entrypoint';
