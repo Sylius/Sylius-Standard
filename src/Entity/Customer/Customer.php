c559219496c858d09b89f736d78455df29c1f7bd
Rafikooo <thecrazyrafik@gmail.com>
2025-05-15 11:56:38 +0200

    Revert "WIP"
    This reverts commit 29ed059ed67b051546204255b5f1455b3b2bb6e6.

--- src/Entity/Customer/Customer.php
+++ src/Entity/Customer/Customer.php
@@ -4,14 +4,11 @@ declare(strict_types=1);
 
 namespace App\Entity\Customer;
 
-use Sylius\B2BKit\Entity\CustomerInterface;
-use Sylius\B2BKit\Entity\CustomerAwareTrait;
 use Doctrine\ORM\Mapping as ORM;
 use Sylius\Component\Core\Model\Customer as BaseCustomer;
 
 #[ORM\Entity]
 #[ORM\Table(name: 'sylius_customer')]
-class Customer extends BaseCustomer implements CustomerInterface
+class Customer extends BaseCustomer
 {
-    use CustomerAwareTrait;
 }
--- src/Entity/Order/Order.php
+++ src/Entity/Order/Order.php
@@ -4,14 +4,11 @@ declare(strict_types=1);
 
 namespace App\Entity\Order;
 
-use Sylius\B2BKit\Entity\OrderInterface;
-use Sylius\B2BKit\Entity\OrderAwareTrait;
 use Doctrine\ORM\Mapping as ORM;
 use Sylius\Component\Core\Model\Order as BaseOrder;
 
 #[ORM\Entity]
 #[ORM\Table(name: 'sylius_order')]
-class Order extends BaseOrder implements OrderInterface
+class Order extends BaseOrder
 {
-    use OrderAwareTrait;
 }
