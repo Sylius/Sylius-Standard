6d9060f24adc85449cf6d7bf601a61cf78318f14
Rafikooo <thecrazyrafik@gmail.com>
2025-05-16 17:50:54 +0200

    wip

--- src/Command/PluginFinalizeCommand.php
+++ src/Command/PluginFinalizeCommand.php
@@ -11,7 +11,6 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 use Symfony\Component\DependencyInjection\Attribute\TaggedIterator;
-use Symfony\Component\Process\Exception\ProcessFailedException;
 use Symfony\Component\Process\Process;
 
 #[AsCommand(
