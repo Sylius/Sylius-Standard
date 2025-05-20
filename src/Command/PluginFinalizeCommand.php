d0c7318c3908c056641c3db4b921d4b5f2d1f23b
Rafikooo <thecrazyrafik@gmail.com>
2025-05-20 13:58:24 +0200

    WIP

--- src/Command/PluginFinalizeCommand.php
+++ src/Command/PluginFinalizeCommand.php
@@ -11,7 +11,7 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 use Symfony\Component\DependencyInjection\Attribute\TaggedIterator;
-use Symfony\Component\DependencyInjection\Exception\ParameterNotFoundException;
+use Symfony\Component\DependencyInjection\Exception\EnvNotFoundException;
 use Symfony\Component\Process\Process;
 
 #[AsCommand(
