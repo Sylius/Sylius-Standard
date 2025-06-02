64d8fe28abb550b9e85cacbe3698959761aca733
Rafikooo <thecrazyrafik@gmail.com>
2025-06-02 15:44:48 +0200

    wip

--- assets/shop/entrypoint.js
+++ assets/shop/entrypoint.js
@@ -1,6 +1,10 @@
 // In this file you can import assets like images or stylesheets
 console.log('Hello Webpack Encore! Edit me in assets/shop/entrypoint.js');
 
+const imagesContext = require.context('./images', true, /\.(jpg|jpeg|png|svg|gif|webp)$/);
+imagesContext.keys().forEach(imagesContext);
+
+
 console.log('🎧 [SHOP] message listener załadowany');
 
 window.addEventListener('message', event => {
