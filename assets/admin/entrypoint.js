546bfd55c31dc7fd155108a63746a75c6cdf1baf
Rafikooo <thecrazyrafik@gmail.com>
2025-06-02 15:43:59 +0200

    wip

--- assets/admin/entrypoint.js
+++ assets/admin/entrypoint.js
@@ -1,6 +1,10 @@
 // In this file you can import assets like images or stylesheets
 console.log('Hello Webpack Encore! Edit me in assets/admin/entrypoint.js');
 
+const imagesContext = require.context('./images', true, /\.(jpg|jpeg|png|svg|gif|webp)$/);
+imagesContext.keys().forEach(imagesContext);
+
+
 // assets/admin/entrypoint.js
 
 console.log('🎧 [ADMIN] message listener załadowany');
