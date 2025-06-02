eab80186316e15b055a4dab18c6a6ff4b4970124
Rafikooo <thecrazyrafik@gmail.com>
2025-06-02 13:30:14 +0200

    wip

--- src/Command/StoreLoader.php
+++ src/Command/StoreLoader.php
@@ -118,6 +118,7 @@ class StoreLoader extends Command
         array $arguments,
         SymfonyStyle $io,
     ): Process {
+        $arguments['--no-debug'] = true;
         $parts = array_merge(["bin/console", $command], $arguments);
         $process = Process::fromShellCommandline(
             implode(' ', $parts),
