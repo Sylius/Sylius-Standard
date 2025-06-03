919f67d5a0e99d1ff9369fe736cf584b3c229808
Rafikooo <thecrazyrafik@gmail.com>
2025-06-03 09:32:57 +0200

    wip

--- src/Command/FixtureLoader.php
+++ src/Command/FixtureLoader.php
@@ -90,6 +90,10 @@ class FixtureLoader extends Command
             $io->warning('No images directory found, skipping image copy.');
         }
 
+        $io->section('[Fixture Loader] Reload cache');
+        $this->runConsoleCommand('cache:clear', [], $io);
+        $this->runConsoleCommand('cache:warmup', [], $io);
+
         $io->text('[Fixture Loader] Rerun fixture-loader command');
         $suite = $data['fixtures']['suite'] ?? 'default';
         $process = $this->runConsoleCommand(
