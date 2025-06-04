5ce2e79b82803f70945939e60aded674d62f116e
Rafikooo <thecrazyrafik@gmail.com>
2025-06-04 17:12:53 +0200

    WIP

--- src/Command/Fixture/FixturePrepare.php
+++ src/Command/Fixture/FixturePrepare.php
@@ -7,6 +7,7 @@ namespace App\Command\Fixture;
 use App\Command\ConfigTrait;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -30,7 +31,8 @@ class FixturePrepare extends Command
 
     protected function configure(): void
     {
-        $this->addArgument('store', InputOption::VALUE_REQUIRED, 'Name of the store directory under store-preset/');
+        $this
+            ->addArgument('store', InputArgument::REQUIRED, 'Name of the store directory under store-preset/');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
@@ -43,22 +45,37 @@ class FixturePrepare extends Command
 
         $result = copy($fixturesPath, $this->projectDir . '/config/packages/fixtures.yaml');
         if (!$result) {
-            $io->error(sprintf('Failed to copy fixtures from %s to %s', $fixturesPath, $this->projectDir . '/config/packages/fixtures.yaml'));
+            $io->error(sprintf(
+                'Failed to copy fixtures from %s to %s',
+                $fixturesPath,
+                $this->projectDir . '/config/packages/fixtures.yaml'
+            ));
             return Command::FAILURE;
         }
 
         $imagesDir = sprintf('%s/store-preset/fixtures/images', $this->projectDir);
         if (is_dir($imagesDir)) {
             $io->section('Copying images');
+
             $destinationDir = $this->projectDir . '/var/fixture_img';
-            $process = Process::fromShellCommandline(
-                sprintf('cp -r %s/* %s', escapeshellarg($imagesDir), escapeshellarg($destinationDir)),
-            );
-            $process->run(
-                function ($type, $buffer) use ($io) {
-                    $io->write($buffer);
+
+            // jeśli katalog docelowy nie istnieje, tworzymy go
+            if (!is_dir($destinationDir)) {
+                if (!mkdir($destinationDir, 0777, true) && !is_dir($destinationDir)) {
+                    $io->error(sprintf('Failed to create directory %s', $destinationDir));
+                    return Command::FAILURE;
                 }
-            );
+            }
+
+            $process = Process::fromShellCommandline(sprintf(
+                'cp -r %s/* %s',
+                escapeshellarg($imagesDir),
+                escapeshellarg($destinationDir)
+            ));
+            $process->run(function ($type, $buffer) use ($io) {
+                $io->write($buffer);
+            });
+
             if ($process->getExitCode() !== 0) {
                 $io->error('Failed to copy images.');
                 return Command::FAILURE;
@@ -67,12 +84,7 @@ class FixturePrepare extends Command
             $io->warning('No images directory found, skipping image copy.');
         }
 
-//        $io->section('[Fixture Loader] Reload cache');
-//        $this->runConsoleCommand('cache:clear', [], $io);
-//        $this->runConsoleCommand('cache:warmup', [], $io);
-
         $io->success('Fixtures prepared successfully.');
-
         return Command::SUCCESS;
     }
 }
