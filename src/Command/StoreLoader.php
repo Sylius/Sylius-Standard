16a92567de1862b209af2f4a178180d4e42eef89
Rafikooo <thecrazyrafik@gmail.com>
2025-06-04 17:53:49 +0200

    WIP

--- src/Command/StoreLoader.php
+++ src/Command/StoreLoader.php
@@ -91,6 +91,9 @@ class StoreLoader extends Command implements BuildAndDeployContextSeparatorInter
 
         $this->io->section('[Store Loader] THEMES');
         $this->runCommand(['composer', 'require', 'intervention/image']);
+        $this->runCommand(['php', 'bin/console', 'cache:clear', '--no-warmup']);
+        $this->runCommand(['php', 'bin/console', 'cache:warmup']);
+
         $this->runCommand(['php', 'bin/console', 'sylius:dx:theme:prepare', $store]);
     }
 
--- src/Command/ThemePrepare.php
+++ src/Command/ThemePrepare.php
@@ -283,6 +283,7 @@ TWIG;
 
         $this->io->text('Clearing cache');
         $process = Process::fromShellCommandline('php bin/console cache:clear', $this->projectDir);
+        $process = Process::fromShellCommandline('php bin/console cache:warmup', $this->projectDir);
         $process->run(fn($type, $buffer) => $this->io->write($buffer));
 
         $this->io->success('[Theme Loader] Theme loading complete!');
