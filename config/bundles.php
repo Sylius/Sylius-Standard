4b0295f78bbb8b4b42322109b03549912e720c72
Rafikooo <thecrazyrafik@gmail.com>
2025-06-09 11:20:08 +0200

    WIP

--- config/bundles.php
+++ config/bundles.php
@@ -62,4 +62,5 @@ return [
     Symfony\UX\Icons\UXIconsBundle::class => ['all' => true],
     FOS\RestBundle\FOSRestBundle::class => ['all' => true],
     Sylius\PayPalPlugin\SyliusPayPalPlugin::class => ['all' => true],
+    Sylius\DXBundle\SyliusDXBundle::class => ['all' => true],
 ];
