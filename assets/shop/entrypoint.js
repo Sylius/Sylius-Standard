8db3547b74182d1e86bfc1e6858e6d993bc5e8bd
Rafikooo <thecrazyrafik@gmail.com>
2025-06-09 15:01:09 +0200

    Add required entrypoint entries

--- assets/shop/entrypoint.js
+++ assets/shop/entrypoint.js
@@ -1,4 +1,7 @@
 import './bootstrap.js';
 
+const imagesContext = require.context('./images', true, /\.(jpg|jpeg|png|svg|gif|webp)$/);
+imagesContext.keys().forEach(imagesContext);
+
 // In this file you can import assets like images or stylesheets
 console.log('Hello Webpack Encore! Edit me in assets/shop/entrypoint.js');
